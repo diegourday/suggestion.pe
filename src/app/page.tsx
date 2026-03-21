e6b93c17db09dd83d0d937559679cdc85f452b7f
Diego Urday <dh7586@outlook.com>
2026-03-20 20:07:53 -0500

    style: standardize import statements to use double quotes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import HeroSection from '@/components/sections/HeroSection';
-import ServicesSection from '@/components/sections/ServicesSection';
-import ValuePropositionSection from '@/components/sections/ValuePropositionSection';
-import PortfolioSection from '@/components/sections/PortfolioSection';
-import TestimonialsSection from '@/components/sections/TestimonialsSection';
-import ProcessSection from '@/components/sections/ProcessSection';
-import BlogSection from '@/components/sections/BlogSection';
-import CTASection from '@/components/sections/CTASection';
+import HeroSection from "@/components/sections/HeroSection";
+import ServicesSection from "@/components/sections/ServicesSection";
+import ValuePropositionSection from "@/components/sections/ValuePropositionSection";
+import PortfolioSection from "@/components/sections/PortfolioSection";
+import TestimonialsSection from "@/components/sections/TestimonialsSection";
+import ProcessSection from "@/components/sections/ProcessSection";
+import BlogSection from "@/components/sections/BlogSection";
+import CTASection from "@/components/sections/CTASection";
 
 export default function Home() {
   return (
